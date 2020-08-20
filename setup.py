d5877e2427bd8a1449694f33a1118f9291a11919
jinsheng <jinsheng@sensetime.com>
2020-08-20 19:06:08 +0800

    clean unnecessary files

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import numpy as np
 ext_modules = [
     Extension(
         'xtcocotools._mask',
-        sources=['../common/maskApi.c', 'xtcocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        sources=['./common/maskApi.c', 'xtcocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), './common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
