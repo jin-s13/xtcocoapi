683dad339a77abe45dd643c19adddcffd2d95986
jinsheng <jinsheng@sensetime.com>
2020-08-20 16:33:53 +0800

    init commit

---
+++ PythonAPI/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, Extension
+import numpy as np
+
+# To compile and install locally run "python setup.py build_ext --inplace"
+# To install library to Python site-packages run "python setup.py build_ext install"
+
+ext_modules = [
+    Extension(
+        'excocotools._mask',
+        sources=['../common/maskApi.c', 'excocotools/_mask.pyx'],
+        include_dirs = [np.get_include(), '../common'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+    )
+]
+
+setup(
+    name='excocotools',
+    packages=['excocotools'],
+    package_dir = {'excocotools': 'excocotools'},
+    install_requires=[
+        'setuptools>=18.0',
+        'cython>=0.27.3',
+        'matplotlib>=2.1.0'
+    ],
+    version='2.0',
+    ext_modules= ext_modules
+)
---
+++ README.txt
@@ -0,0 +1,11 @@
+Extended COCO API (excocotools)
+
+
+COCO has become a standard annotation format for person keypoint detection.
+Our Extended COCO API is developed based on [@cocodataset/cocoapi](https://github.com/cocodataset/cocoapi).
+Our excocotools aims to support multiple human pose-related datasets, including COCO, [CrowdPose](https://github.com/Jeff-sjtu/CrowdPose) and so on.
+
+We provide a simple [demo](PythonAPI/demo_crowdpose.py) to evaluate on CrowdPose dataset.
+
+To install:
+-run "sh install.sh" under coco/PythonAPI
