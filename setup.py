2b62d006933595e308fdd683dda4824db1ee8bd0
jin-s13 <jinsheng13@foxmail.com>
2021-03-22 20:37:49 +0800

    fix compile flags

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ import numpy as np
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
-
+# Note that the original compile flags below are GCC flags unsupported by the Visual C++ 2015 build tools.
+# They can safely be removed.
 ext_modules = [
     Extension(
         'xtcocotools._mask',
         sources=['./common/maskApi.c', 'xtcocotools/_mask.pyx'],
         include_dirs = [np.get_include(), './common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=[] # originally was ['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
@@ -22,7 +23,7 @@ setup(
         'cython>=0.27.3',
         'matplotlib>=2.1.0'
     ],
-    version='1.6',
+    version='1.7',
     description="Extended COCO API",
     url="https://github.com/jin-s13/xtcocoapi",
     ext_modules= ext_modules
--- xtcocotools/coco.py
+++ xtcocotools/coco.py
@@ -49,6 +49,7 @@
 
 import json
 import time
+import matplotlib; matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
